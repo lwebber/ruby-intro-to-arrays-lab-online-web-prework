0446f98ab3a1ce91e7384c87161caf478fa1539a
Laura Webber <wwebby1@gmail.com>
2019-04-23 02:15:27 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  Array.new(2)
+end
+
+def first_element(a)
+  a[0]
+end
+
+def third_element(a)
+  a[2]
+end
+
+def last_element(a)
+  a[a.length-1]
+end
+
+def first_element_with_array_methods(a)
+  a.first
+end
+
+def last_element_with_array_methods(a)
+  a.last
+end
+
+def length_of_array(a)
+  a.length
+end
